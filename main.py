7572650596e034fc50f8ffb8f0a209a142939c6d
awesomo <nickspecht777@gmail.com>
2025-04-28 20:53:11 -0500

    sorting into a list

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from stats import get_num_words
 from stats import get_char_count
+from stats import sort_list
 
 def main():
     count = get_num_words("books/frankenstein.txt")
     char_count_dict = get_char_count("books/frankenstein.txt")
-    print(f"{count} words found in the document")
-    print(char_count_dict)
+    sorted_chars =  sort_list(char_count_dict)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        if char.isalpha():
+            print(f"{char}: {char_dict['num']}")
+    print("============= END ===============")
     return
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,4 @@
 def get_num_words(file_path):
-    file_contents = ""
     with open(file_path) as f:
         text = f.read()
     count = 0
@@ -9,7 +8,6 @@ def get_num_words(file_path):
     return count
 
 def get_char_count(file_path):
-    file_contents = ""
     char_count_dict = {}
     with open(file_path) as f:
         text = f.read()
@@ -18,3 +16,9 @@ def get_char_count(file_path):
         char_count_dict[char_lowercase] = char_count_dict.get(char_lowercase, 0) +1
     return char_count_dict
 
+def sort_list(char_count_dict):
+    char_list = []
+    for key, value in char_count_dict.items():
+        char_list.append({"char": key, "num": value})
+    char_list.sort(key=lambda item: item["num"], reverse=True)
+    return char_list
